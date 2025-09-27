6f50044f27e03a805bf2e6ec7ef7ae68f85a96a6
Voyzer10 <geraansh20@gmail.com>
2025-09-28 00:12:37 +0530

    modified user.js

--- backend/model/User.js
+++ backend/model/User.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
 
+function generateUserId() {
+  // Generates a random 5-digit number between 10000 and 99999
+  return Math.floor(10000 + Math.random() * 90000);
+}
+
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   userId: { 
-    type: mongoose.Schema.Types.ObjectId, 
+    type: Number, 
     required: true, 
     unique: true,
-    default: () => new mongoose.Types.ObjectId() // auto-generate
+    default: generateUserId
   },
 });
 
