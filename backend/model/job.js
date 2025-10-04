c70ac13f80027aaa734e529acd2a87a7ca599bcd
Voyzer10 <geraansh20@gmail.com>
2025-10-04 22:18:39 +0530

    updated job model

--- backend/model/job.js
+++ backend/model/job.js
@@ -2,13 +2,22 @@ const mongoose = require('mongoose');
 
 const JobSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: { type: String },
   location: { type: String },
+  postedAt: { type: Date, default: Date.now },
+  applicantsCount: { type: Number, default: 0 },
   salary: { type: String },
+  benefits: { type: [String] },
+  descriptionText: { type: String },
+  descriptionHtml: { type: String },
+  seniorityLevel: { type: String },
+  employmentType: { type: String },
+  jobFunction: { type: String },
+  industries: { type: [String] },
+  link: { type: String },
+  applyUrl: { type: String },
+  inputUrl: { type: String },
   companyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
-  posterId: { type: mongoose.Schema.Types.ObjectId, ref: 'JobPoster', required: true },
-  createdAt: { type: Date, default: Date.now },
-  status: { type: String, default: 'open' }, // open, closed, etc.
+  jobPosterId: { type: mongoose.Schema.Types.ObjectId, ref: 'JobPoster', required: true }
 });
 
 module.exports = mongoose.models.Job || mongoose.model('Job', JobSchema);
