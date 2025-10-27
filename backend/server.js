38cee15f6e4eda50b9267016a972d327e213e17b
Voyzer10 <geraansh20@gmail.com>
2025-10-27 18:11:36 +0530

    changes deployment config

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 // Middleware
 app.use(cors({
- origin: process.env.CLIENT_URL || "http://localhost:3000",
+ origin: process.env.CLIENT_URL || "https://code-for-aiagent-39q5.vercel.app",
   credentials: true
 }));
 
