6f6cfb70b5db22123313f5e4ac1c023e81f306b2
Ansh Gera <geraansh20@gmail.com>
2025-11-05 18:24:27 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // ✅ CORS FIRST — must be before any routes
 const allowedOrigins = [
-  // "https://code-for-aiagent-39q5.vercel.app",
+  "https://techm.work.gd/",
   "http://localhost:3000", // optional for local dev
 ];
 
