d44597f2b02113950d7ad7da4aa650acc586945d
Ansh Gera <geraansh20@gmail.com>
2025-11-06 01:09:27 +0530

    updation

--- frontend/src/app/auth/register/page.js
+++ frontend/src/app/auth/register/page.js
@@ -28,6 +28,7 @@ export default function RegisterPage() {
     }
 
     const handleSubmit = async (e) => {
+        const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
         e.preventDefault()
         if (formData.password !== formData.confirmPassword) {
             alert('Passwords do not match')
